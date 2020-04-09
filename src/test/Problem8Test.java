c7d88b8a9838425b5612d40e19c293767368879b
Ayoub Rossi <ayoub.rossi@student.kuleuven.be>
2020-04-09 18:52:23 +0200

    Added problem 8 ( non-clean solution )

---
+++ src/test/Problem8Test.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import project.euler.Problem8;
+
+public class Problem8Test {
+
+    Problem8 problem8 = new Problem8();
+
+    @Test
+    public void logSolution() {
+        System.out.println(problem8.getGreatestProduct());
+    }
+}
